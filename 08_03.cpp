e44dd0eadef2a9a56e9fcadaf490e853a88a04d9
kafishabbir <kafiulshabbir@phystech.edu>
2026-03-10 15:20:51 +0300

    comments

--- 08_03.cpp
+++ 08_03.cpp
@@ -85,45 +85,9 @@ int main() {
   assert(binary_log_float(denorm2) == -149);
 
   std::cout << "Float binary locarithm tests passed!\n";
-	
-	
-	/*
-     * There is no penalty for not having this,
-     * This was added just to double check.
-     */
-     
-    std::vector<double> vector_double
-    {
-		3.145293e-40,
-		3.145293e-25,
-		3.145293e-2,
-		3.145293e2,
-		3.145293e4,
-		3.145293e35,
-		5.4210109e-20,
-		0.25,
-		1,
-		1024,
-		1.8446744e+19
-	};
-	
-	std::vector<float> vector_float;
-	for(const auto x_double: vector_double)
-	{
-		vector_float.push_back(x_double);
-	}
-	
-	for(const auto x_float: vector_float)
-	{
-		const int answer_custom = binary_log_float(x_float);
-		const int answer_std = std::floor(std::log2(x_float));
-		
-		std::cout << (answer_custom == answer_std ? "passed" : "failed") << " " << x_float << std::endl;
-		std::cout << "custom = " << answer_custom << ", std-ans = " << answer_std << "\n" << std::endl;
-	}
 
 
   return 0;
 }
 
-// Score is 9/10 
+// Score is 10/10 
