7a746522cf01873b7f04ad9de09d1ecb203b3d45
Roman Farforov <rfarforov@example.com>
2026-04-20 16:07:18 +0300

    Add Task 11.03: QuickSort with custom comparators

---
+++ 11_03.cpp
@@ -0,0 +1,71 @@
+#include <algorithm>
+#include <cmath>
+#include <functional>
+#include <iostream>
+#include <iterator>
+#include <vector>
+
+template <typename Iter, typename Compare = std::less<typename std::iterator_traits<Iter>::value_type>>
+Iter partition_iter(Iter first, Iter last, Compare comp = Compare()) {
+  auto pivot_iter = std::prev(last);
+  auto pivot = *pivot_iter;
+  auto i = first;
+
+  for (auto j = first; j != pivot_iter; ++j) {
+    if (comp(*j, pivot)) {
+      std::iter_swap(i, j);
+      std::advance(i, 1);
+    }
+  }
+  std::iter_swap(i, pivot_iter);
+  return i;
+}
+
+template <typename Iter, typename Compare = std::less<typename std::iterator_traits<Iter>::value_type>>
+void quickSort_iter(Iter first, Iter last, Compare comp = Compare()) {
+  if (std::distance(first, last) > 1) {
+    auto pi = partition_iter(first, last, comp);
+    quickSort_iter(first, pi, comp);
+    quickSort_iter(std::next(pi), last, comp);
+  }
+}
+
+bool compare_descending(int a, int b) { return a > b; }
+
+int main() {
+  std::cout << "--- Task 11.03: Custom Comparators in QuickSort ---" << std::endl;
+
+  std::vector<int> arr = {34, 7, 23, 32, 5, 62, 32, 7, 0};
+
+  std::cout << "Original vector: ";
+  for (int x : arr)
+    std::cout << x << " ";
+  std::cout << "\n\n";
+
+  // 1. Passing standard functional object std::less
+  std::vector<int> arr1 = arr;
+  quickSort_iter(arr1.begin(), arr1.end(), std::less<int>());
+  std::cout << "1. Sorted with std::less:             ";
+  for (int x : arr1)
+    std::cout << x << " ";
+  std::cout << "\n";
+
+  // 2. Passing custom free function
+  std::vector<int> arr2 = arr;
+  quickSort_iter(arr2.begin(), arr2.end(), compare_descending);
+  std::cout << "2. Sorted with free function (desc):  ";
+  for (int x : arr2)
+    std::cout << x << " ";
+  std::cout << "\n";
+
+  // 3. Passing custom lambda function (by absolute value)
+  std::vector<int> arr3 = {10, -5, 20, -30, 0, 5};
+  quickSort_iter(arr3.begin(), arr3.end(),
+                 [](int a, int b) { return std::abs(a) < std::abs(b); });
+  std::cout << "3. Absolute sort with lambda:         ";
+  for (int x : arr3)
+    std::cout << x << " ";
+  std::cout << "\n";
+
+  return 0;
+}
