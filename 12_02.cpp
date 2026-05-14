bef7d4c0145ec7e3f10f31573ba851389a5cce6d
Roman Farforov <rfarforov@example.com>
2026-05-14 23:28:02 +0300

    Implement Task 12.02: Self-printing program (Quine) using std::printf

---
+++ 12_02.cpp
@@ -0,0 +1,7 @@
+#include <cstdio>
+
+int main() {
+    const char* s = "#include <cstdio>%c%cint main() {%c    const char* s = %c%s%c;%c    std::printf(s, 10, 10, 10, 34, s, 34, 10, 10, 10, 10);%c    return 0;%c}%c";
+    std::printf(s, 10, 10, 10, 34, s, 34, 10, 10, 10, 10);
+    return 0;
+}
